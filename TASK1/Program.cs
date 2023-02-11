e1257c33a7bcb7df3bf901a7b3ac9034e13b52fa
Serge Volynets <sergej-volynec@yandex.ru>
2023-02-11 12:47:15 +0300

    Task1

---
+++ TASK1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter first number: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+
+if (FirstNumber != SecondNumber)
+{
+    if (FirstNumber > SecondNumber)
+    {
+        Console.WriteLine("First number is larger.");
+    }
+    else
+    {
+        Console.WriteLine("Second number is larger.");
+    }
+}
+else
+{
+    Console.WriteLine("First number equals second number.");
+}
