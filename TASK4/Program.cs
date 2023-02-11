b6833d551607e697556f5a8297da4668a02c5bbf
Serge Volynets <sergej-volynec@yandex.ru>
2023-02-11 13:32:09 +0300

    Task4

---
+++ TASK4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+//Если введут отрицательное, поменяем знак
+if (Number < 0) Number = Number * (-1);
+
+int count = 2;
+
+while (count <= Number)
+{
+    Console.Write(count + " ");
+    count = count + 2;
+}
+
+
