1bee30303cc1c7fc1ab718c67579076aefb3ff5e
Serge Volynets <sergej-volynec@yandex.ru>
2023-02-11 12:58:15 +0300

    Task2

---
+++ TASK2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter first number: ");
+int FirstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int SecondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter third number: ");
+int ThirdNumber = Convert.ToInt32(Console.ReadLine());
+
+int Max = FirstNumber;
+
+if (SecondNumber > Max) Max = SecondNumber;
+if (ThirdNumber > Max) Max = ThirdNumber;
+
+Console.Write("Max number is: " + Max);
