7934ce3a68e4e2f5998b97e09cdbe120a168821b
Serge Volynets <sergej-volynec@yandex.ru>
2023-02-11 13:13:18 +0300

    Task3

---
+++ TASK3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number != 0)
+{
+    if (Number % 2 != 0) Console.WriteLine("Number is odd.");
+    else Console.WriteLine("Number is even.");
+}
+
+
